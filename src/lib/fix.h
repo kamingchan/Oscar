2fd6fd1732894340b5b7c93ebb64c5c29463718a
bazingaterry <terry@32ph.com>
2016-05-16 23:00:13 +0800

    bug fix

--- src/lib/fix.h
+++ src/lib/fix.h
@@ -37,7 +37,7 @@ typedef int fix;
 
 /* Parameter: fix; Return type: int; rounding to nearest; */
 #define F_INT_N(A) (A > 0 ? F_INT_Z(A + (1 << (SHIFT_BIT - 1))) \
-                          : F_INT_Z(A - (1 << (SHIFT_BIT - 1)))
+                          : F_INT_Z(A - (1 << (SHIFT_BIT - 1))))
 
 /* More information: http://www.ccs.neu.edu/home/amislove/teaching/cs5600/fall10/pintos/pintos_7.html */
 
